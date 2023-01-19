21a6c031f95ac27665bcdef18946d760260a1f28
chiragawasthi <65921425+chiragawasthi@users.noreply.github.com>
2023-01-19 14:41:30 +0530

    Update ViewController.swift

--- NASAPICS/NASAPICS/ViewController.swift
+++ NASAPICS/NASAPICS/ViewController.swift
@@ -10,33 +10,33 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
+    
     //variable declaration
     let appDelegate = UIApplication.shared.delegate as! AppDelegate //Singlton instance
     var context:NSManagedObjectContext!
-  
     let datePicker = UIDatePicker()
     @IBOutlet var Imageapod: UIImageView!
     @IBOutlet var DescriptionText: UITextView!
     @IBOutlet var DateText: UITextField!
+    
+    //main function
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         createDatePicker()
-        
     }
     
+    //this button action navigates us to the favouriteViewController
     @IBAction func FAV(_ sender: Any) {
         let s = self.storyboard?.instantiateViewController(withIdentifier: "FavouriteViewController") as! FavouriteViewController
         self.navigationController?.pushViewController(s, animated: true)
     }
     
-   
-    
-    
+    //this button action calls the opendatabse() function
     @IBAction func ADDTOFAVOURITE(_ sender: Any) {
         openDatabse()
     }
-    // MARK: Methods to Open, Store and Fetch data
+    
+    //this function creates the context to access the core data and then callls the savedata function
     func openDatabse()
     {
         context = appDelegate.persistentContainer.viewContext
@@ -47,6 +47,8 @@ class ViewController: UIViewController {
         
         
     }
+    
+    //this function if called can delete all the data of the core data database
     func deletedate(){
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Favourite")
         let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: request)
@@ -61,9 +63,9 @@ class ViewController: UIViewController {
         }
     }
     
+    //saving data in in core data
     func saveData(Date:NSManagedObject)
     {
-
         let str = String(DateText.text!)
         Date.setValue(str, forKey: "date")
         print("Storing Data..")
@@ -72,29 +74,7 @@ class ViewController: UIViewController {
         } catch {
             print("Storing data Failed")
         }
-
-        fetchData()
     }
-    
-    func fetchData()
-    {
-        print("Fetching Data..")
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Favourite")
-        request.returnsObjectsAsFaults = false
-        do {
-            let result = try context.fetch(request)
-            for data in result as! [NSManagedObject] {
-                let Date = data.value(forKey: "date") as! String
-                
-                print("Favourite Date is : " + Date)
-            }
-        } catch {
-            print("Fetching data Failed")
-        }
-    }
-    
-    
-    
     
     //creating date picker
     func createDatePicker() {
@@ -105,13 +85,16 @@ class ViewController: UIViewController {
         //bar button
         let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
          toolbar.setItems([doneBtn], animated: true)
+        
         //asign toolbar
         DateText.inputAccessoryView = toolbar
+        
         //assign datepicker to text field
         DateText.inputView = datePicker
-        
         datePicker.datePickerMode = .date
     }
+    
+    //function that runs after the done button is pressed
     @objc func donePressed(){
         //formater
         let formatter = DateFormatter()
@@ -120,32 +103,31 @@ class ViewController: UIViewController {
         DateText.text = formatter.string(from: datePicker.date)
         self.view.endEditing(true)
         
+        //initialising variables and setting the URL
         var baseurl = "https://api.nasa.gov/planetary/apod?api_key=tY7oOZ2bgCMkpM4Ddeq81f3ZW9CYnownhgp830th"
         let addDate = "&date="+formatter.string(from: datePicker.date)+"&"
         baseurl = baseurl + addDate
         let url = URL(string: baseurl)!
         
-        
-        
+        //Calling the API Using URL sessions
         let task = URLSession.shared.dataTask(with: url) { data,response, error in
             guard let data =  data else { return }
             let jsonData = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
             let img = jsonData?["url"] as? String
             let desc = jsonData?["explanation"] as? String
             
+            //load image and description on ui asynchronously on the main thread
             DispatchQueue.main.async{
-                
                 self.Imageapod.loadFrom(URLAddress: img!)
                 self.DescriptionText.text = desc!
             }
         }
-        
         task.resume()
-        
     }
-    
+    //end of donePressed function
 }
 
+//creating extension for the UIImageView to load the data in the UIView
 extension UIImageView {
     func loadFrom(URLAddress: String) {
         guard let url = URL(string: URLAddress) else {
@@ -161,6 +143,3 @@ extension UIImageView {
         }
     }
 }
-
-
-
