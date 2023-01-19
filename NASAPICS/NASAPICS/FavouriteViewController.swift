a90884b5cbe9f0caae61c588d1e13c3d9e651f52
chiragawasthi <65921425+chiragawasthi@users.noreply.github.com>
2023-01-19 14:42:57 +0530

    Update FavouriteViewController.swift

--- NASAPICS/NASAPICS/FavouriteViewController.swift
+++ NASAPICS/NASAPICS/FavouriteViewController.swift
@@ -10,27 +10,26 @@ import CoreData
 
 class FavouriteViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
 
-
     //variable declaration
     let appDelegate = UIApplication.shared.delegate as! AppDelegate //Singlton instance
     var context:NSManagedObjectContext!
-    //@IBOutlet var Labeltext: UILabel!
 
-
-    
+    //creating table view
     let tableView: UITableView = {
        let table = UITableView()
         table.register(UITableViewCell.self , forCellReuseIdentifier:  "cell")
        return table
     }()
     
+    //creating variable of table favourite
     private var models = [Favourite]()
-    //table row an d cell
+    
+    //table row return number or rows in model
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
 
-    
+    //table cell will return what the cell will contain in the row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let model = models[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
@@ -38,55 +37,65 @@ class FavouriteViewController: UIViewController , UITableViewDelegate, UITableVi
         return cell
     }
     
+    //this function creates a navigation for every row by clicking them
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //tableView.deselectRow(at: indexPath, animated: true)
         performSegue(withIdentifier: "ShowDetail", sender: self)
     }
     
+    //sending the value of that particular row to the next view controller
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let destination = segue.destination as? LastViewController {
             destination.model = models[(tableView.indexPathForSelectedRow?.row)!  ]
         }
     }
     
+    //main function
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //creating table on this view
         title = "favourite Dates"
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
         
-      //fetchData()
+        //calling function openDatabse
         openDatabse()
-        
     }
    
-    
+    //this function creates context to intract with core data
     func openDatabse()
-       {
-        
-        
+    {
            context = appDelegate.persistentContainer.viewContext
-
+            
+           //calling the fetch data function
            fetchData()
+    }
 
-
-       }
-
+    //this function fetches the data from the core data table "favourite"
     func fetchData()
     {
         print("Fetching Data..")
+        
+        //create instance to fetch data
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Favourite")
+        
+        //this line will ensures that all the data is loaded from the persistent store and the fetch request returns full objects.
         request.returnsObjectsAsFaults = false
+        
         do {
             let result = try context.fetch(request)
+            
+            //we assign the models with the value that is fetched from the favourites table
             models = try context.fetch(request) as! [Favourite]
+            
+            //reloading the table after getting the data from the api
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
-            //tableView.reloadData()
+            
+            //checking if the data is fetched successfully on the output window
             for data in result as! [NSManagedObject] {
                 let Date = data.value(forKey: "date") as! String
 
@@ -96,12 +105,5 @@ class FavouriteViewController: UIViewController , UITableViewDelegate, UITableVi
             print("Fetching data Failed")
         }
     }
-
-
-
-
-
+    //end of fetchData()
 }
-
-
-
